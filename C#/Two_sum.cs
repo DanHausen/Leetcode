5b9db42db1e5396322dc23748f456e9d7a97a7b1
DanHausen <danihausen@live.com>
2021-07-29 23:51:37 -0300

    main

--- C#/Two_sum.cs
+++ C#/Two_sum.cs
@@ -1,6 +1,5 @@
 //https://leetcode.com/problems/two-sum/
 
-<<<<<<< HEAD
 using System;
 
 int[] TwoSumReview(int[] nums, int target){
@@ -21,19 +20,3 @@ int[] TwoSumReview(int[] nums, int target){
 
   return null;
 }
-=======
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-// You can return the answer in any order.
-
-public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        int[] sum = new int[2];
-        for(int i = 0; i < nums.Length; i++){
-        }
-        return sum;
-    }
-}
-
-//TODO
->>>>>>> d5a87e865e64b813d65ac2cb490ee31c7c93213a
