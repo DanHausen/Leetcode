4b476a49dcfffd1461a2fd125ee9edfbbeea6e1e
DanHausen <danihausen@live.com>
2021-07-29 23:22:59 -0300

    Learning

--- C#/Two_sum.cs
+++ C#/Two_sum.cs
@@ -1,2 +1,22 @@
 //https://leetcode.com/problems/two-sum/
 
+using System;
+
+int[] TwoSumReview(int[] nums, int target){
+  Dictionary<int, int> numsDictionary = new Dictionary<int, int>();
+
+  for (int i = 0; i < nums.Length; i++){
+    int num = nums[i];
+
+    // You can uncomment this if nums are all positive
+    //if (num > target) { continue; }
+
+    if (numsDictionary.TryGetValue(target - num, out int index)){          
+      return new [] { index, i }; 
+    }
+
+    numsDictionary[num] = i;
+  }
+
+  return null;
+}
